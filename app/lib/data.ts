c55247cde9e827eb247750da00b65ce37e80cfa7
Ziggy Chen <ziggycs9796@hotmail.com>
2023-11-13 21:48:12 +0800

    debounce pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,6 @@ export async function fetchRevenue() {
     // Don't do this in real life :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -36,8 +35,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore()
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
